f4e58ff508311fbca9f659d0c827d2be43e13626
koyaware <rundelchaos@gmail.com>
2023-07-14 01:31:56 +0500

    code restructure

--- tgbot/handlers/admins/admin.py
+++ tgbot/handlers/admins/admin.py
@@ -1,11 +1,16 @@
 from aiogram import Dispatcher
 from aiogram.types import Message
 
+from tgbot.config import BASE_DIR
 from tgbot.filters import AdminFilter
+from tgbot.misc.keyboards import main_info, mainMenu
+from tgbot.misc.text import start_message
 
 
 async def admin_start(message: Message):
-    await message.reply('Hello, admin!')
+    await message.bot.send_message(message.from_user.id, text=start_message, reply_markup=main_info)
+    with open(BASE_DIR / 'contents/info.MP4', 'rb') as gif_file:
+        await message.bot.send_animation(message.chat.id, gif_file, reply_markup=mainMenu)
 
 
 def register_admin(dp: Dispatcher):

2c89180a498aaed174c9ff7b6a82e5d6eac91e29
koyaware <rundelchaos@gmail.com>
2023-07-16 23:51:03 +0500

    added rates State

--- tgbot/misc/states.py
+++ tgbot/misc/states.py
@@ -4,3 +4,8 @@ from aiogram.dispatcher.filters.state import StatesGroup, State
 class FeedbackState(StatesGroup):
     waiting_for_message = State()
     waiting_for_admin_message = State()
+
+
+class RatesState(StatesGroup):
+    message = State()
+    ban_id = State()
